e28d2127c29de452ae26f701bb95f809093b5684
Ono Takashi <t.ono@onocom.net>
2018-05-31 00:23:36 +0900

    initial commit

---
+++ plugin.php
@@ -0,0 +1,183 @@
+<?php
+/*
+Plugin Name: Gutenberg challenge introduction
+Plugin URI: http://onocom.net/
+Description:  Gutenberg challenge introduction. 
+Author: onocom
+Author URI: http://onocom.net/
+Version: 1.0.0
+
+License:
+ Released under the GPL license
+  http://www.gnu.org/copyleft/gpl.html
+  Copyright 2018 Takashi Ono
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+class gutenberg_challenge_introduction {
+	
+	static $instance = null;
+	
+	protected  $page_templates;
+	
+	
+	function __construct(){
+		self::$instance = $this;
+		
+		$this->page_templates = array(
+									"template-introduction.php" => 'Gutenberg challenge introduction'
+								);
+		add_filter(
+			'wp_insert_post_data', 
+			array( $this, 'register_project_templates' ) 
+		);
+
+
+		// Add a filter to the template include to determine if the page has our 
+		// template assigned and return it's path
+		add_filter('template_include', array( $this, 'view_project_template'));
+		
+		add_filter('theme_page_templates', array( $this, 'add_new_template' ));
+		
+		register_activation_hook(__FILE__, array($this, 'activate'));
+		register_deactivation_hook(__FILE__, array($this, 'deactivate'));
+	}
+	
+	
+	public function activate() {
+		
+	}
+
+	public function deactivate() {
+		
+	}
+
+	public static function get_instance() {
+		
+		if( self::$instance != null ) {
+			return self::$instance;
+		} else {
+			return new self();
+		}
+	}
+
+	public function get_plugin_template_part($slug, $name = null) {
+		$slug;
+		
+		$templates = array();
+		$name = (string) $name;
+		if ( '' !== $name )
+			$templates[] = "{$slug}-{$name}.php";
+		
+		$templates[] = "{$slug}.php";
+		$located = '';
+		foreach ( (array) $templates as $template_name ) {
+			if ( !$template_name ) {
+				continue;
+			}
+			if ( file_exists($this->get_plugin_path() . '/' . $template_name) ) {
+				$located = $this->get_plugin_path() . '/' . $template_name;
+				break;
+			}
+		}
+		if($located) {
+			require_once( $located );
+		}
+	}
+	public function get_plugin_url() {
+		return plugins_url("",__FILE__);
+	}
+	public function get_plugin_path() {
+		return dirname(__FILE__);
+	}
+
+	/**
+	 * http://www.wpexplorer.com/wordpress-page-templates-plugin/
+	 */
+	public function add_new_template( $page_templates ) {
+		$page_templates = array_merge( $page_templates, $this->page_templates );
+		return $page_templates;
+	}
+
+	public function get_edit_link() {
+		if( is_user_logged_in() ) : ?>
+			<a href="<?php echo esc_url( get_site_url()."/wp-admin/post-new.php" );?>" class="btn btn-secondary btn-lg">Gutenberg 編集画面へ</a>
+		<?php  else : ?>
+			<h2 class="display-5">LOGIN</h2>
+			<p><a href="<?php echo esc_url( get_site_url() . "/wp-login.php?redirect_to=" . get_site_url() . "/wp-admin/post-new.php" );?>" class="btn btn-secondary">ログイン画面</a></p>
+			<p><small>username: demo / password: wcosaka2018</small></p>
+		<?php  endif; 
+	}
+	
+	public function register_project_templates( $atts ) {
+
+		// Create the key used for the themes cache
+		$cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
+
+		// Retrieve the cache list. 
+		// If it doesn't exist, or it's empty prepare an array
+		$templates = wp_get_theme()->get_page_templates();
+		if ( empty( $templates ) ) {
+			$templates = array();
+		} 
+
+		// New cache, therefore remove the old one
+		wp_cache_delete( $cache_key , 'themes');
+
+		// Now add our template to the list of templates by merging our templates
+		// with the existing templates array from the cache.
+		$templates = array_merge( $templates, $this->page_templates );
+
+		// Add the modified cache to allow WordPress to pick it up for listing
+		// available templates
+		wp_cache_add( $cache_key, $templates, 'themes', 1800 );
+
+		return $atts;
+
+	} 
+	public function view_project_template( $template ) {
+		
+		// Get global post
+		global $post;
+
+		// Return template if post is empty
+		if ( ! $post ) {
+			return $template;
+		}
+
+		// Return default template if we don't have a custom one defined
+		if ( ! isset( $this->page_templates[get_post_meta( 
+			$post->ID, '_wp_page_template', true 
+		)] ) ) {
+			return $template;
+		} 
+
+		$file = plugin_dir_path( __FILE__ ) . get_post_meta( $post->ID, '_wp_page_template', true );
+
+		// Just to be safe, we check if the file exist first
+		if ( file_exists( $file ) ) {
+			return $file;
+		} else {
+			echo $file;
+		}
+
+		// Return template
+		return $template;
+
+	}
+	
+	
+}
+new gutenberg_challenge_introduction();
---
+++ template-introduction.php
@@ -0,0 +1,93 @@
+<?php
+/*
+Template Name: Gutenberg Challenge Introduction
+*/
+$gci = gutenberg_challenge_introduction::get_instance();
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<title>WordCamp Osaka 2018 Gutenberg Demo Page</title>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	<link href="<?php echo $gci->get_plugin_url();?>/css/style.css" rel="stylesheet">
+</head>
+<body>
+	<div class="p-1 p-md-5 text-center text-white gci-bg-primary">
+		<div class="col-md-12 mx-auto my-5 py-5">
+			<h1 class="display-3 font-weight-normal gci-webfont">Gutenberg<br>DEMO SITE</h1>
+			<p class="font-weight-normal lead">Gutenbergの使い方を覚えたり、機能について理解を深めるために、実際に手を動かしてみましょう。</p>
+			<a href="https://github.com/onocom/gutenberg-challenge/" class="btn btn-light" target="_blank">紹介テーマダウンロード</a>
+		</div>
+	</div>
+	<div class="d-md-flex flex-md-equal w-100 mx-auto ">
+		<div class="w-100 py-5 px-3 text-center">
+			<div class="my-3 py-5">
+				<?php $gci->get_edit_link();?>
+			</div>
+		</div>
+	</div>
+	<div class="p-1 p-md-5 text-center text-white gci-bg-secondary ">
+		<div class="col-md-12 mx-auto my-5">
+			<h2 class="display-3 font-weight-normal gci-webfont">Gutenberg<br>Challenge</h2>
+			<p class="font-weight-normal lead">ランディングページをGutenberg で作ってみよう！</p>
+		</div>
+	</div>
+	<div class="d-md-flex flex-md-equal w-100">
+		<div class="bg-dark text-light w-100 p-3 text-center overflow-hidden">
+			<div class="p-3">
+				<h2 class="display-5">完成イメージ</h2>
+				<p>完成イメージは以下からダウンロードできます</p>
+				<div class="btn-group">
+					<a href="https://github.com/onocom/gutenberg-challenge/raw/master/00_data/step01_check-finish-design-data/pdf/landing-page.pdf" class="btn btn-primary" target="_blank">PDF</a>
+					<a href="https://github.com/onocom/gutenberg-challenge/raw/master/00_data/step01_check-finish-design-data/png/landing-page.png" class="btn btn-primary" target="_blank">PNG</a>
+					<a href="https://github.com/onocom/gutenberg-challenge/raw/master/00_data/step01_check-finish-design-data/xd/landing-page.xd" class="btn btn-primary" target="_blank">Xd</a>
+				</div>
+				<a href="https://github.com/onocom/gutenberg-challenge/" class="btn btn-secondary" target="_blank">GitHub</a>
+
+			</div>
+			<div class="p-5">
+				<div style="max-height: 70vh;overflow: auto;" class="mx-auto">
+					<img src="https://github.com/onocom/gutenberg-challenge/raw/master/00_data/step01_check-finish-design-data/png/landing-page.png" class="img-fluid" alt="完成イメージ" >
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="d-md-flex flex-md-equal w-100 mx-auto ">
+		<div class="w-100 py-5 px-3 text-center">
+			<div class="my-3 py-5">
+				<?php $gci->get_edit_link();?>
+			</div>
+		</div>
+	</div>
+	<div class="d-md-flex flex-md-equal w-100 mx-auto ">
+		<div class="bg-light w-100 py-5 px-3 text-center">
+			<div class="p-5">
+				<h3>誰かさんが公開したページ</h3>
+				<div style="max-width: 600px;max-height: 50vh;overflow: auto;" class="bg-white mx-auto p-0">
+					<div class="m-0 text-left list-group">
+					<?php
+					$posts = get_posts('posts_per_page=5&orderby=modified');
+					foreach( $posts as $the_post ) {
+						echo "<a class='list-group-item' href='" . get_permalink($the_post) . "' target='_blank'>[ID:" . $the_post->ID . "] " . get_permalink($the_post) . "</a>";
+					}
+					?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="d-md-flex flex-md-equal w-100 mx-auto ">
+		<div class="w-100 py-5 px-3 text-center">
+			<a href="http://onocom.net">onocom.net</a>
+		</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+</body>
+</html>
+
